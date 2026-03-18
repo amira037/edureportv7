1c8f4b61c614270f7af59af8c5fb01c7b4b1d967
amira037 <amira037@gmail.com>
2026-03-18 09:44:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-var CACHE = "rpt-v1";
+var CACHE = 'rpt-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap'
+];
 
-self.addEventListener("install", function(e) {
+self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(CACHE).then(function(c) {
-      return c.addAll([
-        "./",
-        "https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap"
-      ]);
+      return Promise.allSettled(ASSETS.map(function(url) {
+        return c.add(url).catch(function() {});
+      }));
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(e) {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
       return Promise.all(
@@ -24,11 +28,17 @@ self.addEventListener("activate", function(e) {
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request).catch(function() {
-        return caches.match("./");
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(res) {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res;
+        var clone = res.clone();
+        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
+        return res;
+      }).catch(function() {
+        return caches.match('./index.html');
       });
     })
   );
